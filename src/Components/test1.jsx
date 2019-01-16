f14bb7d9e041ef274356cd653d80fdab7cf80793
Arnob Almazee <arnobxtreme@gmail.com>
2019-01-16 13:26:31 +0600

    learn day 1

---
+++ src/Components/test1.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Test1 extends Component {
+   data = {
+       count: 0
+   }; 
+
+  render() {
+    return (
+      <div>
+        <p>Arnob</p>
+      </div>
+    );
+  }
+}
